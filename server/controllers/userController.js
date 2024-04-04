1f896041f1e9b83ac000032148947529e8116ec8
chinagonzales <126368606+chinagonzales@users.noreply.github.com>
2024-04-04 10:20:29 +0800

    Add files via upload

---
+++ server/controllers/userController.js
@@ -0,0 +1,62 @@
+const User = require('../models/user');
+
+module.exports = {
+  getAllUsers: async (req, res) => {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+  createUser: async (req, res) => {
+    const user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      role: req.body.role,
+      campus: req.body.campus,
+      hashedPassword: req.body.hashedPassword
+    });
+
+    try {
+      const newUser = await user.save();
+      res.status(201).json(newUser);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+  getUserById: async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+  updateUser: async (req, res) => {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+  deleteUser: async (req, res) => {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json({ message: 'User deleted' });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+};
