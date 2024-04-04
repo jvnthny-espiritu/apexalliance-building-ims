b12a3d055484f71d3eeedcde088604dedacab988
chinagonzales <126368606+chinagonzales@users.noreply.github.com>
2024-04-04 10:23:25 +0800

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-// const usersRoutes = require('./routes/users');
+const usersRoutes = require('./routes/users');
 const buildingsRoutes = require('./routes/buildings');
 const roomsRoutes = require('./routes/rooms');
 const assetsRoutes = require('./routes/assets');
 
-// router.use('/users', usersRoutes);
+router.use('/users', usersRoutes);
 router.use('/buildings', buildingsRoutes);
 router.use('/rooms', roomsRoutes);
 router.use('/assets', assetsRoutes);
