e53357f1cfa503c35c8507e39d4754f518cda250
Jave Anthony Espiritu <96035504+jvnthny-espiritu@users.noreply.github.com>
2024-04-05 13:28:32 +0000

    added building distribution chart

--- client/src/components/Dashboard/BuildingDistribution.jsx
+++ client/src/components/Dashboard/BuildingDistribution.jsx
@@ -1,46 +1,56 @@
 import React from 'react';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Pie } from 'react-chartjs-2';
+import ReactEcharts from "echarts-for-react"; 
 
-ChartJS.register(ArcElement, Tooltip, Legend);
-
-export const data = {
-	labels: ['Pablo Borbon', 'Alangilan', 'ARASOF-Nasugbu', 'Mabini', 'Balayan', 'Lemery', 'San Juan', 'Rosario', 'Lobo', 'JPLPC-Malvar', 'Lipa'],
-	datasets: [
-	  {
-		label: 'No. of Building',
-		data: [12, 19, 3, 5, 2, 3, 10, 4, 5, 2, 9],
-		backgroundColor: [
-			'rgba(0, 202, 220, .5)',
-			'rgba(73, 195, 251, .5)',
-			'rgba(101, 166, 250, .5)',
-			'rgba(126, 128, 231, .5)',
-			'rgba(155, 87, 204, .5)',
-			'rgba(187, 16, 157, .5)',
-			'rgba(208, 0, 95, .5)',
-			'rgba(222, 79, 69, .5)',
-			'rgba(247, 145, 80, .5)',
-			'rgba(255, 203, 118, .5)',
-			'rgba(230, 232, 121, .5)',
-		],
-		borderColor: [
-		  'rgba(0, 202, 220, 1)',
-		  'rgba(73, 195, 251, 1)',
-		  'rgba(101, 166, 250, 1)',
-		  'rgba(126, 128, 231, 1)',
-		  'rgba(155, 87, 204, 1)',
-		  'rgba(187, 16, 157, 1)',
-		  'rgba(208, 0, 95, 1)',
-		  'rgba(222, 79, 69, 1)',
-		  'rgba(247, 145, 80, 1)',
-		  'rgba(255, 203, 118, 1)',
-		  'rgba(230, 232, 121, 1)',
-		],
-		borderWidth: 1,
-	  },
-	],
+export default function BuildingDistribution() {
+    const option = {
+      title: {
+          text: 'Building Distribution',
+          left: 'left',
+          textStyle: {
+              color: '#ffffff'
+          }
+      },
+      tooltip: {
+          trigger: 'item',
+      },
+      legend: {
+          orient: 'vertical',
+          left: 'right',
+          textStyle: {
+              color: '#ffffff'
+          }
+      },
+      series: [
+          {
+              name: 'No. of Building',
+              type: 'pie',
+              label: {
+                color: '#fff'
+              },
+              center: ['30%', '60%'],
+              data: [
+                { value: 1048, name: 'Alangilan' },
+                { value: 735, name: 'Pablo Borbon' },
+                { value: 580, name: 'ARASOF-Nasugbu' },
+                { value: 484, name: 'Balayan' },
+                { value: 300, name: 'Lemery' },
+                { value: 1048, name: 'Mabini' },
+                { value: 735, name: 'JPLPC-Malvar' },
+                { value: 580, name: 'Lipa' },
+                { value: 484, name: 'Rosario' },
+                { value: 300, name: 'San Juan' },
+                { value: 484, name: 'Lobo' }
+              ],
+              emphasis: {
+                  itemStyle: {
+                      shadowBlur: 10,
+                      shadowOffsetX: 0,
+                      shadowColor: 'rgba(0, 0, 0, 0.5)'
+                  }
+              }
+          }
+      ]
   };
   
-export default function BuildingDistribution() {
-	return <Pie data={data} />;
+    return <ReactEcharts option={option} />;
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-// eslint-disable-next-line
 import BuildingDistribution from '../components/Dashboard/BuildingDistribution'; 
 
 export default function Dashboard() {
 	return (
-	  <div className='h-full w-full ml-5'>
+	  <div className='flex-grow h-full mx-10'>
 		<section className='flex h-20 p-5 pb-5 items-center'>
 			<span className='font-mono font-semibold text-xl'>Dashboard</span>
 		</section>
 		<section>
-			<div className='w-80 h-80 p-5 bg-primary rounded-lg'>
+			<div className='w-3/4 h-96 p-5 bg-primary rounded-lg'>
 				<BuildingDistribution />
 			</div>
 		</section>
