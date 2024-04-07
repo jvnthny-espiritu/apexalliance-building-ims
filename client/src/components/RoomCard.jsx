fb7354aafe31f753a77f50505fd1f987b2dec702
Caryll Labay <95457286+caryll-labay@users.noreply.github.com>
2024-04-07 21:12:04 +0800

    modal

--- client/src/components/RoomCard.jsx
+++ client/src/components/RoomCard.jsx
@@ -1,56 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import RoomModal from '../pages/RoomModal';
 
-const RoomCard = ({ room, onClick }) => {
-  const getColorClass = (key) => {
-    switch (key) {
-      case 'laboratory':
-        return 'bg-room-use-laboratory';
-      case 'classroom':
-        return 'bg-room-use-classroom';
-      case 'administrative':
-        return 'bg-room-use-administrative';
-      case 'available':
-        return 'bg-room-use-available';
-      case 'notavailable':
-        return 'bg-room-use-notavailable';
-      default:
-        return '';
-    }
+const RoomCard = ({ room }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const colors = {
+    laboratory: 'bg-purple-600',
+    classroom: 'bg-green-500',
+    administrative: 'bg-pink-500',
+    available: 'bg-blue-500',
+    notavailable: 'bg-green-500'
   };
 
-  const useColorClass = getColorClass(room.use);
-  const statusColorClass = getColorClass(room.status);
+  const useIsPredefined = room.use && colors.hasOwnProperty(room.use);
+  const statusIsPredefined = room.status && colors.hasOwnProperty(room.status);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
   return (
-    <button
-      className="p-5 w-auto bg-primary text-white shadow-md mb-2 m-2 rounded-lg"
-      style={{ 
-        width: '300px', 
-        height: '150px',
-      }}
-    >
-      <div className="flex flex-col justify-start items-start font-body">
-        <h3 className="text-xl font-extrabold">{room.name}</h3>
-        <div className="flex justify-between w-full">
-          <p className="mb-2"> Dimension</p>
-          <p className="mb-2 text-sm ">{room.dimension}</p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p className="mb-2">Use</p>
-          <p className={`p-1 mb-2 rounded-full text-center text-sm ${useColorClass}`}>
-            {room.use}
-          </p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p className="mb-2">
-            Status
-          </p>
-          <p className={`p-0.5 mb-2 rounded-full text-sm ${statusColorClass}`}>
-            {room.status}
-          </p>
+    <>
+      <div
+        className="p-3 bg-[#041527] text-white shadow-md mb-2 mx-5 rounded-lg cursor-pointer"
+        style={{
+          width: '300px',
+          height: '150px',
+        }}
+        onClick={toggleModal}
+      >
+        <div className="flex flex-col justify-start items-start font-body">
+          <h3 className="text-xl font-extrabold">{room.name}</h3>
+          <div className="flex justify-between w-full">
+            <p className="mb-2"> Dimension</p>
+            <p className="mb-2 ">{room.dimension}</p>
+          </div>
+          <div className="flex justify-between w-full">
+            <p className="mb-2">Use</p>
+            <p className={`px-3 text-center mb-2 rounded-xl ${useIsPredefined ? colors[room.use] : ''}`}>
+              {room.use}
+            </p>
+          </div>
+          <div className="flex justify-between w-full">
+            <p className="mb-2">
+              Status
+            </p>
+            <p className={`px-3 text-center mb-2 rounded-xl ${statusIsPredefined ? colors[room.status] : ''}`}>
+              {room.status}
+            </p>
+          </div>
         </div>
       </div>
-    </button>
+      
+      {isModalOpen && <RoomModal room={room} toggleModal={toggleModal} />}
+    </>
   );
 };
 
