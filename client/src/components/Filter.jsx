554d72d30ee128b01bf0d6c2a655c266b3c5f577
ms-jules <142638895+ms-jules@users.noreply.github.com>
2024-04-09 19:26:41 +0800

    Update Filter.jsx

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -42,7 +42,7 @@ export default function Filter() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-70 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-70 focus:outline-none">
           <div className="py-1">
             {campus.map((campusName, index) => (
               <Menu.Item key={index}>
