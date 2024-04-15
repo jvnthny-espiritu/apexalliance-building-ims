450de24164d92117c3a7d472d49c7fc7b7e3b77e
chinagonzales <126368606+chinagonzales@users.noreply.github.com>
2024-04-15 20:58:33 +0800

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -4,10 +4,14 @@ const usersRoutes = require('./routes/users');
 const buildingsRoutes = require('./routes/buildings');
 const roomsRoutes = require('./routes/rooms');
 const assetsRoutes = require('./routes/assets');
+const dashboardRoutes = require('./routes/dashboard');
+const filteringRoutes = require('./routes/filtering');
 
 router.use('/users', usersRoutes);
 router.use('/buildings', buildingsRoutes);
 router.use('/rooms', roomsRoutes);
 router.use('/assets', assetsRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/filtering', filteringRoutes);
 
 module.exports = router;
