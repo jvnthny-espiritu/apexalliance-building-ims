c7e6ff9761019df98c26a001f08f0b07b705d39a
Jave Anthony Espiritu <96035504+jvnthny-espiritu@users.noreply.github.com>
2024-04-16 07:18:19 +0000

    added minor fixes

--- client/src/components/BuildingCard.jsx
+++ client/src/components/BuildingCard.jsx
@@ -4,25 +4,23 @@ import { Link } from 'react-router-dom';
 
 const BuildingCard = ({ buildingName, year, numStory, totalRooms, uses, imgBuilding }) => {
   return (
-    <div className="building-card card flex-shrink-0 w-full">
-      <Link to={'/catalog/room'} className="card-content p-5 flex flex-col">
-        <div className='rounded-xl overflow-hidden'>
-          <img src={imgBuilding} alt={`${buildingName}`} />
-        </div>
-        <div className='building-name text-white text-[24px] font-black font-nunito-sans'>
+    <div className="w-[300px] h-[150px] my-20">
+      <div className="bg-primary rounded-xl shadow-lg flex-shrink-0">
+      <Link to={'/catalog/room'} className="p-3 md:p-5 flex flex-col">
+        <div className='building-name text-white text-[24px] md:text-2xl lg:text-[24px] font-black font-body mt-3'>
           <span className="flex items-center">
             {buildingName}
-            <MdEdit className="w-6 h-6 ml-2" />
+            <MdEdit className="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7 ml-2" />
           </span>
         </div>
-        <div className='building-details text-white font-nunito-sans text-[14px] mt-3 gap-2'>
+        <div className='building-details text-white font-body text-sm md:text-base lg:text-[14px] mt-2 gap-2'>
           <p>Year of Completion: <span className="ml-1">{year}</span></p>
-          <p>No. of Stories: <span className="ml-10">{numStory}</span></p>
+          <p>No. of Stories: <span className="ml-1 md:ml-10">{numStory}</span></p>
           <p>Total No. of Rooms: <span className="ml-1">{totalRooms}</span></p>
           <p>Uses:</p>
-          <ul className="ml-10">
+          <ul className="ml-1 md:ml-3">
             {uses.map((use, index) => (
-              <li key={index} className={`building-use rounded-full mt-2 text-center shadow-md hover:shadow-lg ${
+              <li key={index} className={`building-use rounded-full mt-1 md:mt-2 text-center shadow-md hover:shadow-lg ${
                 index === 0 ? 'bg-[#00B178]' :
                   index === 1 ? 'bg-[#712EE1]' :
                     'bg-[#F02556]'
@@ -34,8 +32,9 @@ const BuildingCard = ({ buildingName, year, numStory, totalRooms, uses, imgBuild
         </div>
       </Link>
     </div>
+    </div>
+    
   );
 };
-
 
 export default BuildingCard;
--- client/src/pages/BuildingPage.jsx
+++ client/src/pages/BuildingPage.jsx
@@ -1,89 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BuildingCard from '../components/BuildingCard';
 
+
 const buildings = [
-	{
-	  buildingName: 'CICS Building',
-	  year: 1998,
-	  numStory: 5,
-	  totalRooms: 32,
-	  uses: ['Classroom', 'Laboratory', 'Administrative'],
-	  imgBuilding: '/'
-	},
-	{
-	  buildingName: 'CEAFA Building',
-	  year: 1998,
-	  numStory: 5,
-	  totalRooms: 32,
-	  uses: ['Classroom', 'Laboratory', 'Administrative'],
-	  imgBuilding: '/'
-	},
-	{
-	  buildingName: 'CIT Building',
-	  year: 1998,
-	  numStory: 5,
-	  totalRooms: 32,
-	  uses: ['Classroom', 'Laboratory', 'Administrative'],
-	  imgBuilding: '/'
-	},
-	{
-	  buildingName: 'CAFAD Building',
-	  year: 1998,
-	  numStory: 5,
-	  totalRooms: 32,
-	  uses: ['Classroom', 'Laboratory', 'Administrative'],
-	  imgBuilding: '/'
-	},
-	{
-	  buildingName: 'CICS Building',
-	  year: 1998,
-	  numStory: 5,
-	  totalRooms: 32,
-	  uses: ['Classroom', 'Laboratory', 'Administrative'],
-	  imgBuilding: '/'
-	},
-	{
-	  buildingName: 'CEAFA Building',
-	  year: 1998,
-	  numStory: 5,
-	  totalRooms: 32,
-	  uses: ['Classroom', 'Laboratory', 'Administrative'],
-	  imgBuilding: '/'
-	},
-	{
-	  buildingName: 'CIT Building',
-	  year: 1998,
-	  numStory: 5,
-	  totalRooms: 32,
-	  uses: ['Classroom', 'Laboratory', 'Administrative'],
-	  imgBuilding: '/'
-	},
-	{
-	  buildingName: 'CAFAD Building',
-	  year: 1998,
-	  numStory: 5,
-	  totalRooms: 32,
-	  uses: ['Classroom', 'Laboratory', 'Administrative'],
-	  imgBuilding: '/'
-	},
-  ];
+  {
+    buildingName: 'CICS Building',
+    year: 1998,
+    numStory: 5,
+    totalRooms: 32,
+    uses: ['Classroom', 'Laboratory', 'Administrative'],
+    imgBuilding: '/'
+  },
+  {
+    buildingName: 'CEAFA Building',
+    year: 1998,
+    numStory: 5,
+    totalRooms: 32,
+    uses: ['Classroom', 'Laboratory', 'Administrative'],
+    imgBuilding: '/'
+  },
+  {
+    buildingName: 'CIT Building',
+    year: 1998,
+    numStory: 5,
+    totalRooms: 32,
+    uses: ['Classroom', 'Laboratory', 'Administrative'],
+    imgBuilding: '/'
+  },
+  {
+    buildingName: 'CAFAD Building',
+    year: 1998,
+    numStory: 5,
+    totalRooms: 32,
+    uses: ['Classroom', 'Laboratory', 'Administrative'],
+    imgBuilding: '/'
+  },
+  {
+    buildingName: 'CICS Building',
+    year: 1998,
+    numStory: 5,
+    totalRooms: 32,
+    uses: ['Classroom', 'Laboratory', 'Administrative'],
+    imgBuilding: '/'
+  },
+  {
+    buildingName: 'CEAFA Building',
+    year: 1998,
+    numStory: 5,
+    totalRooms: 32,
+    uses: ['Classroom', 'Laboratory', 'Administrative'],
+    imgBuilding: '/'
+  },
+  {
+    buildingName: 'CIT Building',
+    year: 1998,
+    numStory: 5,
+    totalRooms: 32,
+    uses: ['Classroom', 'Laboratory', 'Administrative'],
+    imgBuilding: '/'
+  },
+  {
+    buildingName: 'CAFAD Building',
+    year: 1998,
+    numStory: 5,
+    totalRooms: 32,
+    uses: ['Classroom', 'Laboratory', 'Administrative'],
+    imgBuilding: '/'
+  },
+];
 
 export default function BuildingPage() {
-	return (
-	  <div className="flex-grow justify-center overflow-y-auto h-screen">
-		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mt-5 mx-20">
-		  {buildings.map((building, index) => (
-			<BuildingCard
-			  key={index}
-			  buildingName={building.buildingName}
-			  year={building.year}
-			  numStory={building.numStory}
-			  totalRooms={building.totalRooms}
-			  uses={building.uses}
-			  imgBuilding={building.imgBuilding}
-			/>
-		  ))}
-		</div>
-	  </div>
-	);
-  };
+  return (
+    <div className="building-dashboard overflow-y-auto h-screen">
+      <div className="flex justify-center items-center">
+        <div className="flex flex-wrap justify-center items-center mt-5 mx-5">
+          {buildings.map((building, index) => (
+            <div className="flex-none mx-2 mb-4" key={index}>
+              <BuildingCard
+                buildingName={building.buildingName}
+                year={building.year}
+                numStory={building.numStory}
+                totalRooms={building.totalRooms}
+                uses={building.uses}
+                imgBuilding={building.imgBuilding}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
