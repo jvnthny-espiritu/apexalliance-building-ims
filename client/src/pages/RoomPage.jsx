d3d478bc29860c9d2ef999be228819d6c834d14e
Caryll Labay <21-08209@g.batstate-u.edu.ph>
2024-10-01 23:48:16 +0800

    minor fixes

--- client/src/pages/RoomPage.jsx
+++ client/src/pages/RoomPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams, useLocation, useNavigate } from "react-router-dom";
 import FloorSection from "../components/FloorSection";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaArrowLeft } from "react-icons/fa"; // Back icon from react-icons
+import { FaArrowLeft } from "react-icons/fa";
 import api from "../services/api";
 import AddButton from "../components/AddButton";
 
@@ -14,7 +14,7 @@ function RoomPage() {
   const [selectedStatus, setSelectedStatus] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
   const location = useLocation();
-  const navigate = useNavigate(); // To handle navigation
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchBuildingDetails = async () => {
@@ -31,13 +31,8 @@ function RoomPage() {
         const queryParams = new URLSearchParams(location.search);
         let apiUrl = `/building/${buildingId}/rooms`;
 
-        if (selectedType) {
-          apiUrl += `?type=${selectedType}`;
-        }
-
-        if (selectedStatus) {
-          apiUrl += `${selectedType ? "&" : "?"}status=${selectedStatus}`;
-        }
+        if (selectedType) apiUrl += `?type=${selectedType}`;
+        if (selectedStatus) apiUrl += `${selectedType ? "&" : "?"}status=${selectedStatus}`;
 
         const response = await api.get(apiUrl);
         const floorsArray = Object.keys(response.data).map((floorNumber) => ({
@@ -67,100 +62,125 @@ function RoomPage() {
   };
 
   const handleBack = () => {
-    navigate(-1); // Navigate back to the previous page
+    navigate(-1);
   };
 
   return (
     <div className="h-screen w-auto pb-20">
-      <div className="fixed top-16 left-0 right-0 z-10 bg-white shadow-md">
-        <div className="flex bg-primary justify-between items-center p-1 max-w-screen-auto w-full">
-        <div className="flex items-center space-x-6">
-  {/* Back Button */}
-  <button 
-    onClick={handleBack} 
-    className="mx-10 bg-red-600 text-white font-bold text-xl hover:bg-white hover:border hover:border-gray-300 hover:text-red-600 px-4 py-0 rounded-full transition-all duration-300 flex items-center"
-  >
-    <FaArrowLeft className="mr-2" /> Back
-  </button>
-</div>
-
-          <div className="hidden md:flex items-center space-x-4">
-            <TypeFilter onChange={setSelectedType} />
-            <StatusFilter onChange={setSelectedStatus} />
-            <div className="relative">
-              <input
-                type="text"
-                placeholder="Search rooms..."
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-                className="border border-gray-300 rounded-md px-2 py-1 pl-8 focus:outline-none focus:border-blue-500 text-black"
+      <Header
+        handleBack={handleBack}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        setSelectedType={setSelectedType}
+        setSelectedStatus={setSelectedStatus}
+      />
+      <div className="pt-24 mx-6">
+        <div className="hidden md:flex justify-end mt-8 mb-4">
+          <AddButton onClick={handleAddRoom} />
+        </div>
+        <MobileFilters
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+          setSelectedType={setSelectedType}
+          setSelectedStatus={setSelectedStatus}
+          handleAddRoom={handleAddRoom}
+        />
+        <div className="mx-3">
+          {filteredFloors.length > 0 ? (
+            filteredFloors.map((floor, index) => (
+              <FloorSection
+                key={index}
+                floorName={floor.buildingFloor}
+                rooms={floor.rooms}
+                selectedType={selectedType}
+                selectedStatus={selectedStatus}
               />
-            </div>
+            ))
+          ) : (
+            <p>No rooms found for this building.</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Header({ handleBack, searchQuery, setSearchQuery, setSelectedType, setSelectedStatus }) {
+  return (
+    <div className="fixed top-16 left-0 right-0 z-10 bg-white shadow-md">
+      <div className="flex bg-primary justify-between items-center p-1 max-w-screen-auto w-full">
+        <button
+          onClick={handleBack}
+          className="mx-10 bg-red-600 text-white font-bold text-xl hover:bg-white hover:border hover:border-gray-300 hover:text-red-600 px-4 py-0 rounded-full transition-all duration-300 flex items-center"
+        >
+          <FaArrowLeft className="mr-2" /> Back
+        </button>
+        <div className="hidden md:flex items-center space-x-4">
+          <TypeFilter onChange={setSelectedType} />
+          <StatusFilter onChange={setSelectedStatus} />
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Search rooms..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="border border-gray-300 rounded-md px-2 py-1 pl-8 focus:outline-none focus:border-blue-500 text-black"
+            />
           </div>
         </div>
       </div>
+    </div>
+  );
+}
 
-      <div className="pt-24 mx-6">
-        {/* Add Room button positioned before the floor section */}
-        <div className="flex justify-end mt-8 mb-4">
-          <AddButton onClick={handleAddRoom} />
+function MobileFilters({ searchQuery, setSearchQuery, setSelectedType, setSelectedStatus, handleAddRoom }) {
+  return (
+    <div className="flex flex-wrap ml-3 text-sm md:hidden font-normal relative">
+      <div className="w-full mb-4 mt-5">
+        <div className="relative">
+          <AiOutlineSearch className="absolute top-1/2 left-2 transform -translate-y-1/2 text-gray-400 text-sm md:text-base lg:text-lg" />
+          <input
+            type="text"
+            placeholder="Search rooms..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="border border-gray-300 rounded-md pl-8 pr-2 py-1 focus:outline-none focus:border-blue-500 text-black text-sm md:text-base lg:text-lg w-full"
+          />
         </div>
-
-        {/* Floor sections */}
-        {filteredFloors.length > 0 ? (
-          filteredFloors.map((floor, index) => (
-            <FloorSection
-              key={index}
-              floorName={floor.buildingFloor}
-              rooms={floor.rooms}
-              selectedType={selectedType}
-              selectedStatus={selectedStatus}
-            />
-          ))
-        ) : (
-          <p>No rooms found for this building.</p>
-        )}
+      </div>
+      <div className="flex items-center space-x-4 w-full mb-4">
+        <TypeFilter onChange={setSelectedType} />
+        <StatusFilter onChange={setSelectedStatus} />
+        <AddButton onClick={handleAddRoom} />
       </div>
     </div>
   );
 }
 
 function TypeFilter({ onChange }) {
-  const handleTypeChange = (e) => {
-    onChange(e.target.value);
-  };
-
   return (
-    <div className="flex items-center space-x-2">
-      <select
-        className="border border-gray-300 rounded-md px-2 py-1 focus:outline-none focus:border-blue-500 text-black"
-        onChange={handleTypeChange}
-      >
-        <option value="">All Types</option>
-        <option value="Classroom">Classroom</option>
-        <option value="Laboratory">Laboratory</option>
-        <option value="Administrative">Administrative</option>
-      </select>
-    </div>
+    <select
+      className="border border-gray-300 rounded-md px-2 py-1 focus:outline-none focus:border-blue-500 text-black"
+      onChange={(e) => onChange(e.target.value)}
+    >
+      <option value="">All Types</option>
+      <option value="Classroom">Classroom</option>
+      <option value="Laboratory">Laboratory</option>
+      <option value="Administrative">Administrative</option>
+    </select>
   );
 }
 
 function StatusFilter({ onChange }) {
-  const handleStatusChange = (e) => {
-    onChange(e.target.value);
-  };
-
   return (
-    <div className="flex items-center space-x-2">
-      <select
-        className="border border-gray-300 rounded-md px-2 py-1 focus:outline-none focus:border-blue-500 text-black"
-        onChange={handleStatusChange}
-      >
-        <option value="">All Statuses</option>
-        <option value="Available">Available</option>
-        <option value="Not Available">Not Available</option>
-      </select>
-    </div>
+    <select
+      className="border border-gray-300 rounded-md px-2 py-1 focus:outline-none focus:border-blue-500 text-black"
+      onChange={(e) => onChange(e.target.value)}
+    >
+      <option value="">All Statuses</option>
+      <option value="Available">Available</option>
+      <option value="Not Available">Not Available</option>
+    </select>
   );
 }
 
