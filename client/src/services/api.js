8a5d859483fb46cb2a77608103b01e83f6b161b9
jvnthny-espiritu <jvnthny.espiritu@gmail.com>
2024-11-26 13:07:01 +0800

    Add env variable to react app API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+require('dotenv').config();
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/'
+  baseURL: (process.env.REACT_APP_API_URL) || 'http://localhost:5000/'
 });
 
 api.interceptors.request.use((config) => {
